56dc193c95c648f82b108789cecfd349c8f62861
Brandon <hello@Brandon.com>
2022-05-29 15:22:53 -0500

    added login and fixed signup bug, __init__ when database update, structure and procfile added

--- app/models.py
+++ app/models.py
@@ -44,7 +44,7 @@ class User(db.Model, UserMixin):
         return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
-        return f'<Usuario: {self.user_name}'
+        return f'<Usuario: {self.useheroku}'
 
 
 @login_manager.user_loader
